70b50afb12d27667d23ff9d1d796fcd8428d3c62
Alejandro Camarillo <alejandro.camarillo@wizeline.com>
2019-10-13 22:00:04 -0500

    chore: Prevents empty messages

--- utils.js
+++ utils.js
@@ -7,20 +7,22 @@ const cache = {};
 
 const intervalToMS = (interval) => interval * 1000 || 10000;
 
-const getNewItems = source =>
+const newItems = source =>
   source.filter(({ questionId }) => {
     const isNew = !cache.hasOwnProperty(questionId);
     if (isNew) cache[questionId] = questionId;
     return isNew;
   });
 
-const postMessage = (source, webhook, messageGenerator) =>
-  rp({
+const postMessage = (source, webhook, messageGenerator) => {
+  if (newItems().length === 0) return;
+  return rp({
     method: 'POST',
     uri: webhook,
-    body: messageGenerator(source, getNewItems),
+    body: messageGenerator(source, newItems),
     json: true
   });
+}
 
 module.exports = {
   intervalToMS,
